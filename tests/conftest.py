901abea8a22996b5324d4573989d555385550900
Marcin Sobczyk <marcinpiotrsobczyk@gmail.com>
2024-03-25 23:51:12 +0100

    add autouse fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,8 @@ import pytest
 @pytest.fixture()
 def some_string() -> str:
     return "some string"
+
+
+@pytest.fixture(autouse=True, scope="session")
+def some_string2() -> str:
+    return "some string2"
