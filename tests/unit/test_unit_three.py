93f13c5960137fc9f521592b9dd17df4415f42cd
Marcin Sobczyk <marcinpiotrsobczyk@gmail.com>
2024-03-26 01:38:31 +0100

    suppres beautifulsoup warning

--- tests/unit/test_unit_three.py
+++ tests/unit/test_unit_three.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 
 import logging
+import warnings
 from pathlib import Path
 
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, XMLParsedAsHTMLWarning
 
 
 def test_case_beautifulsoup() -> None:
+    warnings.filterwarnings("ignore", category=XMLParsedAsHTMLWarning
+
     logging.error("from unit_one suite")
     example_file_path = Path(__file__).parent / "example.xml"
     with open(example_file_path) as file:
