158fdd329023a7e690d786e332d600c9aa18c477
Marcin Sobczyk <marcinpiotrsobczyk@gmail.com>
2024-03-28 15:00:01 +0100

    some code generated by codeium

--- python_boilerplate/__private/one.py
+++ python_boilerplate/__private/one.py
@@ -3,3 +3,15 @@ from __future__ import annotations
 
 def get_hello_string() -> str:
     return "Hello World!"
+
+class One:
+    def __init__(self) -> None:
+        pass
+
+class Two:
+    def __init__(self) -> None:
+        pass
+
+class Card:
+    def __init__(self) -> None:
+        pass
